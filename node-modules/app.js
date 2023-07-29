6336d16de331cc5015f49901a3c31523490e04a3
AbijuruT <abijuruyves@gmail.com>
2023-07-29 22:12:43 +0200

    About node module

--- node-modules/app.js
+++ node-modules/app.js
@@ -1,43 +1,57 @@
-/** Working with file */
-// const path = require('path');
-// var pathObj = path.parse(__filename);
+// /** Working with file */
+// // const path = require('path');
+// // var pathObj = path.parse(__filename);
 
-// console.log(pathObj)
+// // console.log(pathObj)
 
-/** working with files */
-// const os = require('os');
-// var totalMemory = os.freemem()
+// /** working with files */
+// // const os = require('os');
+// // var totalMemory = os.freemem()
 
-// console.log(totalMemory)
+// // console.log(totalMemory)
 
-/** Working with file */
-// const fs = require('fs');
+// /** Working with file */
+// // const fs = require('fs');
 
-// // const files = fs.readdirSync('./');
-// // console.log(files)
+// // // const files = fs.readdirSync('./');
+// // // console.log(files)
 
-// fs.readdir('../../', (error, files) => {
-//     if (error) console.log('Error', error)
-//     else {
-//         files.forEach(file => {
-//             console.log(file)
-//         })
-//     }
+// // fs.readdir('../../', (error, files) => {
+// //     if (error) console.log('Error', error)
+// //     else {
+// //         files.forEach(file => {
+// //             console.log(file)
+// //         })
+// //     }
+// // })
+// /** Event module */
+
+// const EventEmitter = require('events')
+
+
+// /** Register an event */
+
+// const Logger = require('./logger')
+// const loggerObj = new Logger()
+// loggerObj.on('messageLogged', arg => {
+//     console.log('Listener called', arg);
 // })
-/** Event module */
+// b
+// loggerObj.log('message')
+// /** Raising an event */
+// // emitter.emit('messageLogged', {id: 1, url: 'http://'});
 
-const EventEmitter = require('events')
+const http = require('http');
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello World');
+        res.end();
+    }
+    if (req.url === '/api/myghees/') {
+        res.write(JSON.stringify(['Hommy', 'Placido', 'Godson']))
+        res.end()
+    }
+});
 
-
-/** Register an event */
-
-
-const Logger = require('./logger')
-const loggerObj = new Logger()
-loggerObj.on('messageLogged', arg => {
-    console.log('Listener called', arg);
-})
-
-loggerObj.log('message')
-/** Raising an event */
-// emitter.emit('messageLogged', {id: 1, url: 'http://'});
+server.listen(3000)
+console.log('Listening on port mentioned above......')
